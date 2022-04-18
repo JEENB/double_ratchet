969e3be79c2f89a20b6931305dc61a2d57146d8e
JEENB <bjenish12@gmail.com>
2022-04-18 16:20:19 +0530

    DR-offline

---
+++ client.py
@@ -0,0 +1,61 @@
+import base64
+
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+from cryptography.hazmat.primitives.asymmetric.ed25519 import  Ed25519PublicKey, Ed25519PrivateKey
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.backends import default_backend
+
+from Cryptodome.Cipher import AES
+
+import socket, threading
+
+ROOT_KEY = 'I7xv5oMpgFhWhxiLi3cwAAw9onHQmIwis10TdLWC97Q='
+nickname = input("Choose your nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      #socket initialization
+client.connect(('127.0.0.1', 7976))                             #connecting client to server
+
+
+def b64(msg):
+    # base64 encoding helper function
+    return base64.encodebytes(msg).decode('utf-8').strip()
+
+def hkdf(inp, length):
+    # use HKDF on an input to derive a key
+    hkdf = HKDF(algorithm=hashes.SHA256(), length=length, salt=b'',
+                info=b'', backend=default_backend())
+    return hkdf.derive(inp)
+
+class SymmRatchet(object):
+    def __init__(self, key):
+        self.state = key
+
+    def next(self, inp=b''):
+        # turn the ratchet, changing the state and yielding a new key and IV
+        output = hkdf(self.state + inp, 80)
+        self.state = output[:32]
+        outkey, iv = output[32:64], output[64:]
+        return outkey, iv
+
+def receive():
+    while True:                                                 #making valid connection
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICKNAME':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:                                                 #case on wrong ip/port details
+            print("An error occured!")
+            client.close()
+            break
+def write():
+    while True:                                                 #message layout
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode('ascii'))
+
+receive_thread = threading.Thread(target=receive)               #receiving multiple messages
+receive_thread.start()
+write_thread = threading.Thread(target=write)                   #sending messages 
+write_thread.start()
---
+++ server.py
@@ -0,0 +1,46 @@
+#Coded by Yashraj Singh Chouhan
+import socket, threading                                                #Libraries import
+
+host = '127.0.0.1'                                                      #LocalHost
+port = 7976                                                             #Choosing unreserved port
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)              #socket initialization
+server.bind((host, port))                                               #binding host and port to socket
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):                                                 #broadcast function declaration
+    for client in clients:
+        client.send(message)
+
+def handle(client):                                         
+    while True:
+        try:                                                            #recieving valid messages from client
+            message = client.recv(1024)
+            broadcast(message)
+        except:                                                         #removing clients
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def receive():                                                          #accepting multiple clients
+    while True:
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))       
+        client.send('NICKNAME'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+receive()
